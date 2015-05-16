95619733ce6fd127257e4d7a3a600549e80ecdbc
Trevor Prinn <trev@babbacom.com>
2015-05-16 17:15:05 +0100

    Added MultipartMessage test.

--- SockLibUnitTests/MessageTests.cs
+++ SockLibUnitTests/MessageTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,20 @@ using Babbacombe.SockLib;
 namespace SockLibUnitTests {
     [TestClass]
     public class MessageTests {
+        private class TransFile {
+            private Stream _stream;
+            private string _name;
+            public TransFile(string name, Stream stream) {
+                _name = name;
+                _stream = stream;
+            }
+            public void Delete() {
+                _stream.Dispose();
+                File.Delete(_name);
+            }
+        }
+        private static List<TransFile> _tempFiles = new List<TransFile>();
+
         [TestMethod]
         public void TextMessage() {
             testTextMessage("TestText", "abcdefg\r\nxyz");
@@ -26,8 +41,15 @@ namespace SockLibUnitTests {
             reply.Stream.Dispose();
         }
 
-        private RecMessage transferMessage(SendMessage msg) {
-            var s = new MemoryStream();
+        private RecMessage transferMessage(SendMessage msg, bool useFile = false) {
+            Stream s;
+            if (useFile) {
+                var name = Path.GetTempFileName();
+                s = new FileStream(name, FileMode.Create, FileAccess.ReadWrite);
+                _tempFiles.Add(new TransFile(name, s));
+            } else {
+                s = new MemoryStream();
+            }
             msg.Send(s);
             s.Seek(0, SeekOrigin.Begin);
             var ds = new DelimitedStream(s);
@@ -73,6 +95,54 @@ namespace SockLibUnitTests {
             Assert.IsFalse(names.Zip(reply.Filenames, (sn, rn) => sn == rn).Any(r => false));
         }
 
+        [TestMethod]
+        public void MultipartMessage() {
+            var r = new Random();
+            var files = Enumerable.Range(1, 10).Select(i => new RandomFile(r.Next(50 * 1024 * 1024), i % 2 == 0 ? "\r\n" : null)).ToArray();
+            var bins = Enumerable.Range(1, 5).Select(i => {
+                var buf = new byte[r.Next(10 * 1024 * 1024)];
+                r.NextBytes(buf);
+                return new { Name = Path.GetRandomFileName(), Data = buf };
+            }).ToArray();
+            List<SendMultipartMessage.BaseItem> items = new List<SendMultipartMessage.BaseItem>(files.Select(f => new SendMultipartMessage.FileItem(f.Name)));
+            foreach (var bin in bins) items.Insert(r.Next(items.Count + 1), new SendMultipartMessage.BinaryItem(bin.Name, bin.Data));
+            var msg = new SendMultipartMessage("TestMp", items);
+            var reply = (RecMultipartMessage)transferMessage(msg, true);
+            var man = new MultipartManager(reply.Stream);
+            int fcount = 0;
+            int bcount = 0;
+            man.BinaryUploaded += (s, e) => {
+                var bin = bins.Single(b => b.Name == e.Info.Name);
+                var pos = 0;
+                int buflen = 1024 * 1024;
+                var buf = new byte[buflen];
+                int rcount;
+                do {
+                    rcount = e.Contents.Read(buf, 0, buflen);
+                    Assert.IsFalse(bin.Data.Skip(pos).Zip(buf.Take(rcount), (b1, b2) => b1 == b2).Any(res => false));
+                    pos += rcount;
+                } while (rcount > 0);
+                bcount++;
+                Assert.IsTrue(pos == bin.Data.Length);
+            };
+            man.FileUploaded += (s, e) => {
+                var file = files.Single(f => f.Name == e.Info.Filename);
+                fcount++;
+                Assert.IsTrue(file.IsEqual(e.Contents));
+            };
+            man.Process();
+            Assert.AreEqual(10, fcount, "File count");
+            Assert.AreEqual(5, bcount, "Binary count");
+            foreach (var f in files) f.Dispose();
+            reply.Stream.Dispose();
+            cleanUp();
+        }
+
+        private static void cleanUp() {
+            foreach (var f in _tempFiles) f.Delete();
+            _tempFiles.Clear();
+        }
+
         public class RandomFile : IDisposable {
             private FileInfo _info;
 
@@ -114,6 +184,8 @@ namespace SockLibUnitTests {
                 }
                 return true;
             }
+
+            public string Name { get { return _info.FullName; } }
         }
     }
 }
