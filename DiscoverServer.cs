2a7b6b71cdc67e73a92a8a0f24ab59d41cf0b22c
Trevor Prinn <trev@babbacom.com>
2016-03-28 18:03:50 +0100

    Handles exceptions better on closing the Discover server.
    Improved disposal of Discover server.

--- DiscoverServer.cs
+++ DiscoverServer.cs
@@ -70,27 +70,35 @@ namespace Babbacombe.SockLib {
             _advertisment = Encoding.UTF8.GetBytes(string.Format("{0}:{1}", serviceName, servicePort));
 
             _server = new UdpClient(port);
+			_server.EnableBroadcast = true;
             _cancel = new CancellationTokenSource();
 
-            _runTask = Task.Run(() => runServer(_cancel.Token), _cancel.Token);
+			_runTask = Task.Run(() => runServer(_cancel.Token), _cancel.Token);
         }
 
-        private async Task runServer(CancellationToken cancel) {
-            while (!cancel.IsCancellationRequested) {
-                var msg = await _server.ReceiveAsync(cancel);
-                if (!cancel.IsCancellationRequested && msg != null &&
-                    Encoding.UTF8.GetString(msg.Value.Buffer) == _serviceName) {
-                    await _server.SendAsync(_advertisment, msg.Value.RemoteEndPoint, cancel);
-                }
-            }
-            System.Diagnostics.Debug.WriteLine("Server stopped");
-        }
+		private async Task runServer(CancellationToken cancel) {
+			while (!cancel.IsCancellationRequested) {
+				var msg = await _server.ReceiveAsync(cancel);
+				if (!cancel.IsCancellationRequested && msg != null &&
+					Encoding.UTF8.GetString(msg.Value.Buffer) == _serviceName) {
+					await _server.SendAsync(_advertisment, msg.Value.RemoteEndPoint, cancel);
+				}
+			}
+			System.Diagnostics.Debug.WriteLine("Server stopped");
+		}
 
-        public async void Dispose() {
-            _cancel.Cancel();
-            await _runTask;
-            _server.Close();
-            _cancel.Dispose();
+		protected virtual void Dispose(bool disposing) {
+			if (disposing) {
+				_cancel.Cancel();
+				_runTask.Wait();
+				_server.Close();
+				_cancel.Dispose();
+			}
+		}
+
+        public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
         }
     }
 }
